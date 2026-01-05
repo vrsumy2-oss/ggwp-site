a585b291844cea0e296be0cc9ec642dcae9af28c
vrsumy2-oss <vrsumy2@gmail.com>
2026-01-05 14:25:44 +0200

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 from core.views import (
     index,
     login_view,
@@ -27,4 +29,6 @@ urlpatterns = [
     path("items/<int:pk>/", item_detail_view),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
