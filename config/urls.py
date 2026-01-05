c067dc10e14e47761796d94dbf0953a474e0af63
vrsumy2-oss <vrsumy2@gmail.com>
2026-01-05 14:18:40 +0200

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -27,5 +27,4 @@ urlpatterns = [
     path("items/<int:pk>/", item_detail_view),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
